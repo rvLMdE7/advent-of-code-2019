a9c593ceff4762522cb04ab17f3d50a60d739095
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2020-11-29 21:51:09 +0000

    swap order of arguments

--- day-02/Main.hs
+++ day-02/Main.hs
@@ -15,22 +15,22 @@ main = do
     print $ part1 input
 
 part1 :: V.Vector Int -> Int
-part1 prog = runIntcodeProg (prog V.// [(1, 12), (2, 2)]) 0
+part1 prog = runIntcodeProg 0 (prog V.// [(1, 12), (2, 2)])
 
-runIntcodeProg :: V.Vector Int -> Int -> Int
-runIntcodeProg prog iPtr = case prog V.! iPtr of
+runIntcodeProg :: Int -> V.Vector Int -> Int
+runIntcodeProg iPtr prog = case prog V.! iPtr of
     1 ->
       let
         x = prog V.! (iPtr + 1)
         y = prog V.! (iPtr + 2)
       in
-        runIntcodeProg (prog V.// [(iPtr + 3, x + y)]) (iPtr + 4)
+        runIntcodeProg (iPtr + 4) (prog V.// [(iPtr + 3, x + y)])
     2 ->
       let
         x = prog V.! (iPtr + 1)
         y = prog V.! (iPtr + 2)
       in
-        runIntcodeProg (prog V.// [(iPtr + 3, x * y)]) (iPtr + 4)
+        runIntcodeProg (iPtr + 4) (prog V.// [(iPtr + 3, x * y)])
     99 -> prog V.! 0
 
 readUtf8File :: FilePath -> IO T.Text
