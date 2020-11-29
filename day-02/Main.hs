aca13eeda808c5602c75dabd6e185e82214f3274
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2020-11-29 21:45:05 +0000

    first pass at day 1, part 1

---
+++ day-02/Main.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
+import Data.Vector qualified as V
+import Data.ByteString qualified as B
+import Data.Text qualified as T
+import Data.Text.Encoding qualified as TE
+import Data.Text.Read qualified as TR
+import Flow ((.>))
+
+
+main :: IO ()
+main = do
+    input <- parseInput <$> readUtf8File "input.txt"
+    print $ part1 input
+
+part1 :: V.Vector Int -> Int
+part1 prog = runIntcodeProg (prog V.// [(1, 12), (2, 2)]) 0
+
+runIntcodeProg :: V.Vector Int -> Int -> Int
+runIntcodeProg prog iPtr = case prog V.! iPtr of
+    1 ->
+      let
+        x = prog V.! (iPtr + 1)
+        y = prog V.! (iPtr + 2)
+      in
+        runIntcodeProg (prog V.// [(iPtr + 3, x + y)]) (iPtr + 4)
+    2 ->
+      let
+        x = prog V.! (iPtr + 1)
+        y = prog V.! (iPtr + 2)
+      in
+        runIntcodeProg (prog V.// [(iPtr + 3, x * y)]) (iPtr + 4)
+    99 -> prog V.! 0
+
+readUtf8File :: FilePath -> IO T.Text
+readUtf8File path = TE.decodeUtf8 <$> B.readFile path
+
+parseInput :: T.Text -> V.Vector Int
+parseInput = T.lines .> fmap readInt .> V.fromList
+  where
+    readInt :: T.Text -> Int
+    readInt txt = case TR.decimal txt of
+        Left _ -> error "parseInput: input not an Int"
+        Right (n, _) -> n
