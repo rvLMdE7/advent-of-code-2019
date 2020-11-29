d403790dffbb72d932842f1765aff8779c5cc6ad
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2020-11-29 21:22:54 +0000

    day 1, part 1

---
+++ day-01/Main.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
+import Data.ByteString qualified as B
+import Data.Text qualified as T
+import Data.Text.Encoding qualified as TE
+import Data.Text.Read qualified as TR
+import Flow ((.>))
+
+
+main :: IO ()
+main = do
+    input <- parseInput <$> readUtf8File "input.txt"
+    print $ part1 input
+
+part1 :: [Int] -> Int
+part1 = fmap fuelRequiredForMass .> sum
+  where
+    fuelRequiredForMass :: Int -> Int
+    fuelRequiredForMass m = (m `div` 3) - 2
+
+readUtf8File :: FilePath -> IO T.Text
+readUtf8File path = TE.decodeUtf8 <$> B.readFile path
+
+parseInput :: T.Text -> [Int]
+parseInput = T.lines .> fmap readInt
+  where
+    readInt :: T.Text -> Int
+    readInt txt = case TR.decimal txt of
+        Left _ -> error "parseInput: input not an Int"
+        Right (n, _) -> n
