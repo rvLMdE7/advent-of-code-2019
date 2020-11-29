306ab30ea3328b29156a3404eec7531fb69f29d4
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2020-11-29 21:23:11 +0000

    day 1, part 2

--- day-01/Main.hs
+++ day-01/Main.hs
@@ -11,12 +11,23 @@ main :: IO ()
 main = do
     input <- parseInput <$> readUtf8File "input.txt"
     print $ part1 input
+    print $ part2 input
 
 part1 :: [Int] -> Int
 part1 = fmap fuelRequiredForMass .> sum
   where
     fuelRequiredForMass :: Int -> Int
     fuelRequiredForMass m = (m `div` 3) - 2
+
+part2 :: [Int] -> Int
+part2 = fmap fuelRequiredForMass .> sum
+  where
+    fuelRequiredForMass :: Int -> Int
+    fuelRequiredForMass m
+        | fuel <= 0 = 0
+        | otherwise = fuel + fuelRequiredForMass fuel
+      where
+        fuel = (m `div` 3) - 2
 
 readUtf8File :: FilePath -> IO T.Text
 readUtf8File path = TE.decodeUtf8 <$> B.readFile path
