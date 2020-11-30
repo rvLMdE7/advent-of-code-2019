7dcd8eba2f8da1f8c7f16f5afa689dbbf2c51940
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2020-11-30 00:23:16 +0000

    first pass at part 2
    This doesn't end up working, but the reason for that is because the
    exact specification is subtle and we misunderstood the finer details
    of it. Nonetheless, this serves as a first pass. We will try to
    implement the right spec. in our next commit.

--- day-04/Main.hs
+++ day-04/Main.hs
@@ -8,12 +8,22 @@ main :: IO ()
 main = do
     let input = (183_564, 657_474)
     print $ uncurry part1 input
+    print $ uncurry part2 input
 
 part1 :: Int -> Int -> Int
 part1 a b = length [ pw | pw <- [a..b], isPossiblePassword pw ]
 
+part2 :: Int -> Int -> Int
+part2 a b = length [ pw | pw <- [a..b], isVeryPossiblePassword pw ]
+
 isPossiblePassword :: Int -> Bool
 isPossiblePassword n = hasTwoAdjacent digits && isNonDecreasing digits
+  where
+    digits = getDigits n
+
+isVeryPossiblePassword :: Int -> Bool
+isVeryPossiblePassword n =
+    hasTwoAdjacentButNoMore digits && isNonDecreasing digits
   where
     digits = getDigits n
 
@@ -22,6 +32,11 @@ getDigits = show .> fmap (\c -> read [c])
 
 hasTwoAdjacent :: Eq a => [a] -> Bool
 hasTwoAdjacent xs = any (> 1) grouped
+  where
+    grouped = length <$> group xs
+
+hasTwoAdjacentButNoMore :: Eq a => [a] -> Bool
+hasTwoAdjacentButNoMore xs = any (> 1) grouped && all (<= 2) grouped
   where
     grouped = length <$> group xs
 
