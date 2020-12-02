a131e7eed804f3d681a724c1e6130b146116f56f
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2020-12-02 21:43:55 +0000

    do day 8, part 2

--- day-08/Day08.hs
+++ day-08/Day08.hs
@@ -3,6 +3,7 @@ module Day08 where
 import Data.Vector qualified as V
 import Data.ByteString qualified as B
 import Data.Text qualified as T
+import Data.Text.IO qualified as TIO
 import Data.Text.Encoding qualified as TE
 import Data.Char (digitToInt)
 import Flow ((.>))
@@ -18,12 +19,34 @@ main :: IO ()
 main = do
     input <- parseInput (25, 6) <$> readFileUtf8 "day-08/input.txt"
     print $ part1 input
+    TIO.putStr $ part2 input
 
 part1 :: Image Int -> Int
 part1 image = countOccurencesLayer 1 layer * countOccurencesLayer 2 layer
   where
     i = layerWithLeast 0 image
     layer = image V.! i
+
+part2 :: Image Int -> T.Text
+part2 = composeImage overlay .> presentImage .> display
+  where
+    overlay n m = if n == 2 then m else n
+
+presentImage :: Layer Int -> Layer Char
+presentImage = fmap (fmap conv)
+  where
+    conv n
+        | n == 0 = ' '
+        | otherwise = '#'
+
+display :: Layer Char -> T.Text
+display = fmap (V.toList .> T.pack) .> V.toList .> T.unlines
+
+composeImage :: (a -> a -> a) -> Image a -> Layer a
+composeImage f = V.foldr1 $ composeLayers f
+
+composeLayers :: (a -> a -> a) -> Layer a -> Layer a -> Layer a
+composeLayers f = V.zipWith $ V.zipWith f
 
 layerWithLeast :: Eq a => a -> Image a -> Int
 layerWithLeast x =
